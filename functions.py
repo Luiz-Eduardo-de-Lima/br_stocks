ca38db0556628d9297615fce5a83c7dac8dd8a52
Luiz Eduardo <luizeduardodelima4@gmail.com>
2022-06-15 08:16:18 -0300

    organizando

--- functions.py
+++ functions.py
@@ -10,13 +10,18 @@ def get_statements(begin = int, end = int):
     base_url = 'http://dados.cvm.gov.br/dados/CIA_ABERTA/DOC/ITR/DADOS/'
 
     for year in range(begin, end +1):
+        print('Baixando arquivos...')
         wget.download(base_url + f'itr_cia_aberta_{year}.zip')
+        print('Download completo!')
+
         with ZipFile(f'itr_cia_aberta_{year}.zip', 'r') as zip:
-            print(' \nExtraindo arquivos...')
+            print('Extraindo arquivos...')
             zip.extractall('statements')
+
+            print('\nExtração completa!')
             os.system(f'rm -fr itr_cia_aberta_{year}.zip')
         
-    statements = ['BPA','BPP','DFC_MD','DFC_MI','DMPL','DRA','DRE','DVA', '']
+    statements = ['BPA','BPP','DFC_MD','DFC_MI','DMPL','DRA','DRE','DVA']
     statements_type = ['ind', 'con']
 
     for stt in statements:
@@ -25,9 +30,17 @@ def get_statements(begin = int, end = int):
             data = pd.DataFrame()
 
             for year in range(begin, end +1):
-                input_stt = pd.read_csv(f'statements/itr_cia_aberta_{stt}_{stt_tp}_{year}.csv', sep = ';')
-                data = pd.concat(data, input_stt, inplace = True)
+                input_stt = pd.read_csv(f'statements/itr_cia_aberta_{stt}_{stt_tp}_{year}.csv', sep = ';', decimal = ',', encoding='ISO-8859-1')
+                data = pd.concat([data, input_stt])
                 os.system(f'rm -fr statements/itr_cia_aberta_{stt}_{stt_tp}_{year}.csv')
                 
-            data.to_csv(f'statements/{stt}_{stt_tp}_from_{begin}_to_{end}')
+            data.to_csv(f'statements/{stt}_{stt_tp}_from_{begin}_to_{end}.csv', index = False)
+
+    return
+
+def pl_hist(company = str, begin = int, end = int):
+    data = pd.read_csv(f'statements/DRE_con_from_{begin}_to_{end}.csv')
+    company = data['DENOM_CIA' == company]
+
+    pl = []
     return
